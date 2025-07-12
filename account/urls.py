b80c00267444c709abeef9459aa5cff6bf4959df
Girdhar Kumawat <girdharkumawat3108@gmail.com>
2025-07-12 20:49:27 +0530

    Fix formatting of auth endpoint paths in urls.py for consistency

--- account/urls.py
+++ account/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     # Auth endpoints// api/auth/
     path('isauthenticated/', views.check_auth, name='is_authenticated'),
     path('user/register/', views.userRegister, name='user_register'),
-    path('user/login/', view=views.userLogin, name='user_login'),
-    path('user/logout/', view=views.userLogout, name='user_logout'),
-    path('token/refresh/', view=views.cookieTokenRefresh, name='token_refresh'),
-    path('google/', view=views.google_login, name='google-login'),
+    path('user/login/', views.userLogin, name='user_login'),
+    path('user/logout/', views.userLogout, name='user_logout'),
+    path('token/refresh/', views.cookieTokenRefresh, name='token_refresh'),
+    path('google/', views.google_login, name='google-login'),
     # User endpoints
 ]
