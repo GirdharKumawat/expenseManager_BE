27e49b1567306a0c23e73781e30de3239dd36180
GirdharKumawat <girdharkumawat3108@gmail.com>
2026-09-19 15:18:22 +0530

    Update .gitignore and enhance user profile response with first and last names

--- base/views.py
+++ base/views.py
@@ -42,6 +42,9 @@ def getProfile(request):
     profile = {
         'username': user.username,
         'email': user.email,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        
     }
     return Response({'data': profile}, status=status.HTTP_200_OK)
 
