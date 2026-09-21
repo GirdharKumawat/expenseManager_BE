7e5e67edf8b95584905c07f1822069dc62343e24
GirdharKumawat <girdharkumawat3108@gmail.com>
2026-09-21 17:40:20 +0530

    Refactor manage.py to improve module resolution and update requirements.txt

--- manage.py
+++ manage.py
@@ -2,10 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
+    BASE_DIR = Path(__file__).resolve().parent
+    if str(BASE_DIR) not in sys.path:
+        sys.path.insert(0, str(BASE_DIR))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expenseManager_BE.settings')
     try:
         from django.core.management import execute_from_command_line
